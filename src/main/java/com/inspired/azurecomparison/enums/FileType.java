6b47b9267b507312ea079f29bf27cf017217c7ec
bohdan <napadovskibohdan@gmail.com>
2024-02-16 19:33:10 +0200

    working with styles

--- src/main/java/com/inspired/azurecomparison/enums/FileType.java
+++ src/main/java/com/inspired/azurecomparison/enums/FileType.java
@@ -2,7 +2,9 @@ package com.inspired.azurecomparison.enums;
 
 import org.apache.commons.io.FilenameUtils;
 
+import java.io.File;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public enum FileType {
 
@@ -14,8 +16,8 @@ public enum FileType {
 
     public static FileType getFileTypeByFileName(String fileName) {
         String extension = FilenameUtils.getExtension(fileName);
-        return Arrays.stream(FileType.values()).toList()
-                .stream().filter(t-> t.toString().equalsIgnoreCase(extension))
+        return Arrays.stream(FileType.values())
+                .filter(t-> t.toString().equalsIgnoreCase(extension))
                 .findAny()
                 .orElseThrow();
     }
